084f2946d147518198e9f04fa542fdbcc707fbfa
Denis Gruia <denis.gruiax@icloud.com>
2025-02-05 20:19:29 +0200

    Bubble sort implementend and a simple unit test added

---
+++ src/sort/src/bubble.rs
@@ -0,0 +1,31 @@
+fn sort<T>(v: &Vec<T>) -> Vec<T>
+where
+    T: PartialOrd + Clone,
+{
+    let mut acc = v.clone();
+    let len = acc.len() - 1;
+
+    for _ in 0..len {
+        for j in 0..len {
+            match acc[j] > acc[j + 1] {
+                true => acc.swap(j, j + 1),
+                false => (),
+            }
+        }
+    }
+
+    acc
+}
+
+#[cfg(test)]
+mod tests {
+    use super::sort;
+    use std::vec;
+
+    #[test]
+    fn sort_simple_vector() {
+        let v = vec![10, 7, 9, 18, 9, 13];
+        let v = sort(&v);
+        assert_eq!(v, vec![7, 9, 9, 10, 13, 18]);
+    }
+}
