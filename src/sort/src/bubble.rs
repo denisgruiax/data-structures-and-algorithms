b584a66a80fbd237e971d8153c0514335560913b
Denis Gruia <denis.gruiax@icloud.com>
2025-02-05 20:30:02 +0200

    Added one unit test and modified the sort code in order to work with empty vectors

--- src/sort/src/bubble.rs
+++ src/sort/src/bubble.rs
@@ -3,7 +3,13 @@ where
     T: PartialOrd + Clone,
 {
     let mut acc = v.clone();
-    let len = acc.len() - 1;
+    let mut len = acc.len();
+
+    if len == 0{
+        return acc;
+    }else {
+        len-=1;
+    }
 
     for _ in 0..len {
         for j in 0..len {
@@ -21,11 +27,19 @@ where
 mod tests {
     use super::sort;
     use std::vec;
+    #[test]
+    fn sort_empty_vector() {
+        let v: Vec<i32> = vec![];
+        let v = sort(&v);
+
+        assert_eq!(v, vec![]);
+    }
 
     #[test]
     fn sort_simple_vector() {
         let v = vec![10, 7, 9, 18, 9, 13];
         let v = sort(&v);
+        
         assert_eq!(v, vec![7, 9, 9, 10, 13, 18]);
     }
 }
